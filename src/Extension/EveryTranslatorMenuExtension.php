7fb6147bd9af5446d4bfd91a8fd9408f354f1ef7
Walid Awad <walid@awad.at>
2024-09-09 08:37:52 +0200

    Initial commit v1.0

---
+++ src/Extension/EveryTranslatorMenuExtension.php
@@ -0,0 +1,13 @@
+<?php
+namespace EveryTranslator\Extension;
+
+use SilverStripe\ORM\DataExtension;
+use EveryTranslator\Helper\EveryTranslatorHelper;
+
+class EveryTranslatorMenuExtension extends DataExtension {
+
+    public function onAfterWrite() {
+        parent::onAfterWrite();
+        EveryTranslatorHelper::createDefault($this->owner->Title);
+    }
+}
